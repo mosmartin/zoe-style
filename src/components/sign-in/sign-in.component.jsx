4200d36087611b7a55c9bddef408e0efaf59bb95
Martin Macharia <mosmartin5@gmail.com>
2020-12-19 00:28:27 +0100

    Update sign-in.component.jsx
    Sign in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import { Component } from 'react';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -16,13 +16,21 @@ class SignIn extends Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
 
-    this.setState({
-      email: '',
-      password: '',
-    });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+
+      this.setState({
+        email: '',
+        password: '',
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 
   handleChange = (event) => {
@@ -60,7 +68,7 @@ class SignIn extends Component {
             <CustomButton type="submit"> Sign in </CustomButton>
 
             <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-              Google Sign In 
+              Google Sign In
             </CustomButton>
           </div>
         </form>
