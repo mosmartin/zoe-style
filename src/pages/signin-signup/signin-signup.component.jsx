c787729d63dca140aa4c8f9a1fcb5f435cc5252e
Martin Macharia <mosmartin5@gmail.com>
2020-12-19 00:20:29 +0100

    Store authenticated users in firebase

--- src/pages/signin-signup/signin-signup.component.jsx
+++ src/pages/signin-signup/signin-signup.component.jsx
@@ -1,10 +1,12 @@
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 import './signin-signup.styles.scss';
 
 const SigninSignupPage = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
